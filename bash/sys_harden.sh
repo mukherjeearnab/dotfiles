283189b91a6c8e8441792aa130769234d1a272d5
Arnab Mukherjee <35556016+mukherjeearnab@users.noreply.github.com>
2023-02-15 13:10:36 +0530

    Updated Script with Message Logging

--- bash/sys_harden.sh
+++ bash/sys_harden.sh
@@ -1,90 +1,110 @@
+#####################
+# Auxiliary Methods #
+#####################
+function executeStep() {
+    BBlue='\033[1;34m'
+    BYellow='\033[1;33m'
+    Green='\033[0;32m'
+    Cyan='\033[0;36m'
+    Color_Off='\033[0m'
+    echo "$Cyan STEP: $2"
+    echo "$BYellow Executing: $Green $1 $Color_off"
+    eval $1
+    echo "$BBlue Completed: $Green $1 $Color_off"
+}
+
+
 ######################
 # 1. Firewall Config #
 ######################
 # enable ufw firewall and deny incomming traffic to all open ports
-ufw enable
+executeStep('ufw enable', 'Enable Firewall')
 
 # check firewall status
-ufw status
+executeStep('ufw status', 'Firewall Status Check')
 
 
 ###########################
 # 2. Disable Root Account #
 ###########################
 # disable password for root user
-passwd -l root
+executeStep('passwd -l root', 'Disable ROOT user password')
 
 
 #################
 # 3. SSH Config #
 #################
 # install ssh if not installed
-apt install openssh-server -y
+executeStep('apt install openssh-server -y', 'Install OpenSSH Server')
 
 # check ssh status
-systemctl status ssh
+executeStep('systemctl status ssh', 'Check SSH Status')
 
 # enable ssh
-systemctl enable ssh
+executeStep('systemctl enable ssh', 'Enable SSH')
 
 # allow ssh through firewall
-ufw allow ssh
+executeStep('ufw allow ssh', 'Allow SSH through Firewall')
 
 # disable ssh root user login
     # backup ssh config
-    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+    executeStep('cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup', 'Backup SSH Config')
 
     # if PermitRootLogin is commented, uncomment
-    sed -i  's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
+    executeStep('sed -i  "s/#PermitRootLogin/PermitRootLogin/" /etc/ssh/sshd_config', 'Disable ROOT SSH login #1')
+    
     # if PermitRootLogin is yes, set it to no
-    sed -i  's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+    executeStep('sed -i  "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config', 'Disable ROOT SSH login #2')
 
 # restart ssh service
-systemctl restart ssh
+executeStep('systemctl restart ssh', 'Restart SSH Service')
 
 
 #############################
 # 4. Update Package Manager #
 #############################
 # run apt update and upgrade
-apt update
-apt upgrade
+executeStep('apt update', 'Update APT Repositories')
+executeStep('apt upgrade -y', 'Update Installed Packages')
 
 
 ######################
 # 5. Fail2Ban Config #
 ######################
 # install fail2ban
-apt install fail2ban -y
+executeStep('apt install fail2ban -y', 'Install Fail2Ban')
 
 # copy fail2ban config as .local
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+executeStep('cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local', 'Backup Fail2Ban Config')
+
 
 # check fail2ban service and enable it
-systemctl status fail2ban.service
-systemctl enable --now fail2ban.service
+executeStep('systemctl status fail2ban.service', 'Check Fail2Ban Service Status')
+executeStep('systemctl enable --now fail2ban.service', 'Enable Fail2Ban Service')
 
 # update fail2ban config
     # set ban increment to true
-    sed -i 's/#bantime.increment/bantime.increment/' /etc/fail2ban/jail.local
-
+    executeStep('sed -i "s/#bantime.increment/bantime.increment/" /etc/fail2ban/jail.local', 'Enable Fail2Ban Ban-time Increment')
+    
     # uncomment ban factor
-    sed -i 's/#bantime.factor/bantime.factor/' /etc/fail2ban/jail.local
+    executeStep('sed -i "s/#bantime.factor/bantime.factor/" /etc/fail2ban/jail.local', 'Enable Fail2Ban Ban-time Mult Factor')
     # set ban factor to 2
-    sed -i 's/bantime.factor = 1/bantime.factor = 2/' /etc/fail2ban/jail.local
+    executeStep('sed -i "s/bantime.factor = 1/bantime.factor = 2/" /etc/fail2ban/jail.local', 'Set Mult Factor to 2')
+    
 
 # restart fail2ban service
-systemctl restart fail2ban.service
+executeStep('systemctl restart fail2ban.service', 'Restart Fail2Ban Service')
 
 # check fail2ban service status
-systemctl status fail2ban.service
+executeStep('systemctl status fail2ban.service', 'Check Fail2Ban Service Status')
+
 
 
 ###################
 # 6. Final Checks #
 ###################
 # finally check open ports if any
-ss -antp
+executeStep('ss -antp', 'Check for Open Ports')
 
 # check firewall status
-ufw status
+executeStep('ufw status', 'Check Firewall Status')
